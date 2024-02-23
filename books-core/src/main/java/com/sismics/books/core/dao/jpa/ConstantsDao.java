b7c6b1516f6cbd7d76a23bc2478686d7221148b4
priyanshul-govil <priyanshul.amity9i@gmail.com>
2024-02-23 20:23:36 +0530

    Initial commit

---
+++ books-core/src/main/java/com/sismics/books/core/dao/jpa/ConstantsDao.java
@@ -0,0 +1,18 @@
+package com.sismics.books.core.dao.jpa;
+
+public class ConstantsDao {
+    public static final String id = "id";
+    public static final String name = "name";
+    public static final String username = "username";
+    public static final String tagId = "tagId";
+    public static final String userId = "userId";
+    public static final String userBookId = "userBookId";
+    public static final String bookId = "bookId";
+    public static final String appId = "appId";
+    public static final String deleteDate = "deleteDate";
+    public static final String longLasted = "longLasted";
+    public static final String minDate = "minDate";
+    public static final String currentDate = "currentDate";
+    public static final String updateDate = "updateDate";
+    public static final String passwordResetKey = "passwordResetKey";
+}
